aac1461cdc4e6cb8a40494f327185982e52fdebb
almaskilichov-prog <itsmywaytoamerica@gmail.com>
2022-05-26 11:16:17 +0300

    Initial commit

---
+++ app/src/main/java/kz/almas/comixread/MainActivity.java
@@ -0,0 +1,91 @@
+package kz.almas.comixread;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+import kz.almas.comixread.fragments.AboutFragment;
+import kz.almas.comixread.fragments.CatalogFragment;
+import kz.almas.comixread.fragments.HomeFragment;
+import kz.almas.comixread.fragments.ProfileFragment;
+import kz.almas.comixread.fragments.SearchFragment;
+
+public class MainActivity extends AppCompatActivity {
+    // Инициализация переменных
+    MeowBottomNavigation bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Присваивание переменных
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        // Добавление пунктом меню (items)
+        bottomNavigation.add(new MeowBottomNavigation.Model(0, R.drawable.ic_profile));
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_catalog));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_search));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_about));
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                // Инициализация фрагментов
+                Fragment fragment = null;
+                // Проверка условий
+                switch (item.getId()) {
+                    case 0:
+                        // Инициализация фрагмента страницы профиля
+                        fragment = new ProfileFragment();
+                        break;
+                    case 1:
+                        // Инициализация фрагмента главной страницы
+                        fragment = new HomeFragment();
+                        break;
+                    case 2:
+                        // Инициализация фрагмента страницы каталога
+                        fragment = new CatalogFragment();
+                        break;
+                    case 3:
+                        // Инициализация фрагмента страницы поиска
+                        fragment = new SearchFragment();
+                        break;
+                    case 4:
+                        // Инициализация фрагмена страницы о нас
+                        fragment = new AboutFragment();
+                        break;
+                }
+                // Загрузка фрагмента
+                loadFragment(fragment);
+            }
+        });
+
+        bottomNavigation.show(1, true);
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText(getApplicationContext(), "You clicked " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText(getApplicationContext(), "You reselected " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .commit();
+    }
+}
---
+++ app/src/main/java/kz/almas/comixread/fragments/ProfileFragment.java
@@ -0,0 +1,89 @@
+package kz.almas.comixread.fragments;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import kz.almas.comixread.R;
+
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        // обработка нажатия на аватар
+        CardView changeAvatar = view.findViewById(R.id.defaultAvatar_cardView);
+        changeAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] options = new String[]{"Сделать фото", "Выбрать из галереи", "Отмена"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Выберите ваш аватар");
+
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (i == 0) {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(intent, 0);
+                        } else if (i == 1) {
+                            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(intent, 1);
+                        }
+                    }
+                });
+
+                AlertDialog alertDialog = builder.show();
+                alertDialog.getWindow().setGravity(Gravity.CENTER);
+            }
+
+
+        });
+
+        // обработка на нажатие кнопки "редактировать профиль"
+        CardView editProfile_cardView = view.findViewById(R.id.editProfile_cardView);
+        editProfile_cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment editProfileFragment = new EditProfileFragment();
+                loadFragment(editProfileFragment);
+            }
+        });
+
+        return view;
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(getContext(), "Работает", Toast.LENGTH_SHORT).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+}
